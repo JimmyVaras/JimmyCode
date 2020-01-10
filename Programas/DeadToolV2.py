a3617fac50f2e07a5802f0f1a106d8295249e4d0
JimmyVaras LaptopW7 <35966995+JimmyVaras@users.noreply.github.com>
2020-01-10 18:38:48 +0100

    Data file + (end->sleep&close)

--- Programas/DeadToolV2.py
+++ Programas/DeadToolV2.py
@@ -1,11 +1,12 @@
 import random
 import math
 import pickle
+import time
 
 try:
   data = open("data","rb")
   objeto = pickle.load(data)
-  print(f"Tus objetos son: {objeto[0]}, {objeto[1]} y unas {objeto[2]}")
+  #print(f"Tus objetos son: {objeto[0]}, {objeto[1]} y unas {objeto[2]}")
   data.close()
 except:
   nuevos = []
@@ -44,20 +45,25 @@ print("Para aumentar en 1 la tristeza escribe 'trist' y para la chungez 'chung'.
 print("Para ver tu inventario escribe 'inventario'. ")
 print("Para ver tus estadisticas escribe 'stats'. ")
 print("Para empezar un enfrentamiento escribe 'batalla' ")
+print("Para finalizar el programa usa: `end´ ")
+
 def prop(com):
 	global inv1
 	global inv2
 	global inv3
 	global trist
 	global chung
+
 	if com == "1":
 		inv1 = str(input(f"Escribe el nuevo objeto para sustituir a tu {inv1}: "))
 	elif com == "2":
 		inv2 = str(input(f"Escribe el nuevo objeto para sustituir a tu {inv2}: "))
 	elif com == "3":
 		inv3 = str(input(f"Escribe el nuevo objeto para sustituir a tu {inv3}: "))	
+
 	elif com == "dado":
-		print(dado())		
+		print(dado())	
+
 	elif com == "batalla":
 		enemigo = int(input("Introduce las tiradas del enemigo: "))
 		dp1 = random.randint(1,6)
@@ -73,22 +79,28 @@ def prop(com):
 		else:
 			print("Ganas el enfretamiento")
 	elif com == "stats":
-		print(f"Tu puntos de Tristeza actuales son {trist} y tienes {chung} puntos de chunguéz.")		
+		print(f"Tu puntos de Tristeza actuales son {trist} y tienes {chung} puntos de chunguéz.")	
+
 	elif com == "trist":
 		trist+= 1
 		print(f"Tu tristeza esta en {trist}.")
+
 	elif com == "chung":
 		chung+= 1
 		print(f"Tu chunguéz esta en {chung}.")
-	
+
 	elif com == "end":
 		nuevos = [inv1,inv2,inv3]
 		data = open("data","wb")
 		pickle.dump(nuevos,data)
 		data.close()
+		time.sleep(0.75)
+		quit()
 		print("Se han guardado tus datos, programa finalizado")
+
 	else:
 		print("Acción inexistente, intentalo de nuevo.")
 		
+
 while True:	
 	prop(str(input("Que quieres hacer? -")))
