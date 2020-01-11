53d5c9729e16b4daca87b665e0c871dfa7146120
JimmyVaras LaptopW7 <35966995+JimmyVaras@users.noreply.github.com>
2020-01-11 20:36:00 +0100

    Create fechas y horas.py

---
+++ Apuntes Basicos/fechas y horas.py
@@ -0,0 +1,16 @@
+import datetime
+
+meses = {1:"Enero", 2:"Febrero", 3:"Marzo", 4:"Abril", 5:"Mayo", 6:"Junio", 7:"Julio", 8:"Agosto", \
+9:"Septiembre", 10:"Octubre", 11:"Noviembre", 12:"Diciembre"}
+
+today = datetime.date.today()
+
+print(f"Hoy es día {today.day} de {meses[(today.month)]} del año {today.year}")
+
+cumple = datetime.date(2005, 5, 5)
+
+
+print(f"Tu cumple es el día {cumple.day} de {meses[(cumple.month)]} del año {cumple.year}.\n")
+
+
+
